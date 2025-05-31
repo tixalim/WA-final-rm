fe5b2b0f87c67c9d12d685eac2a0ef160b6867ee
tixalim <rosalie.musilova@gmail.com>
2025-05-31 12:16:25 +0200

    #2 logout

--- controllers/logout.php
+++ controllers/logout.php
@@ -3,5 +3,6 @@ session_start();
 session_unset();
 session_destroy();
 
-header("Location: ../public/index.php");
+require_once __DIR__ . '/../config/config.php';
+header("Location: " . BASE_URL . "public/index.php");
 exit();
